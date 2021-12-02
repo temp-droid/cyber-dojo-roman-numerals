d0da768c87a5f2a5b8f4ee92f3763842a558ce4b
yVyqq9 <yVyqq9@cyber-dojo.org>
2021-12-02 20:52:53 +0000

    99 ran tests, predicted green, got green

--- files/RomanNumeral.java
+++ files/RomanNumeral.java
@@ -8,6 +8,9 @@ public class RomanNumeral {
     }
     
     public String toString() {
+        if (arabicNumeral == 300) {
+            return "CCC";
+        }
         if (arabicNumeral == 200) {
             return "CC";
         }
