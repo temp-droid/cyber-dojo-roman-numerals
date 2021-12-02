e2e446a217cd5fce5659212ee321ae9ff2b8ce5a
yVyqq9 <yVyqq9@cyber-dojo.org>
2021-12-02 20:43:29 +0000

    85 ran tests, predicted green, got red

--- files/RomanNumeral.java
+++ files/RomanNumeral.java
@@ -21,10 +21,27 @@ public class RomanNumeral {
             return "X".repeat(arabicNumeral / 10);
         }
         if (arabicNumeral > 10) {
-            return "X" + digit(arabicNumeral - 10, "I", "V", "X");
+            return ten(arabicNumeral / 10, "X", "L", "C") + digit(arabicNumeral / 10, "I", "V", "X");
         }
         
         return digit(arabicNumeral, "I", "V", "X");
+    }
+    
+    private String ten(int arabicNumeral, String digit, String five, String ten) {
+        switch (arabicNumeral) {
+            case 1: case 2: case 3:
+                return digit.repeat(arabicNumeral);
+            case 4:
+                return digit + five;
+            case 5:
+                return five;
+            case 6: case 7: case 8:
+                return five + digit.repeat(arabicNumeral - 5);
+            case 9:
+                return digit + ten;
+            default:
+                return "";
+        }
     }
     
     private String digit(int arabicNumeral, String digit, String five, String ten) {
