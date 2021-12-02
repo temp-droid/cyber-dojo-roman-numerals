55ce1abcc72df7ba568edba8862c8e05b916574a
yVyqq9 <yVyqq9@cyber-dojo.org>
2021-12-02 19:54:49 +0000

    13 ran tests, predicted amber, got red

--- files/RomanNumeralTest.java
+++ files/RomanNumeralTest.java
@@ -5,8 +5,21 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class RomanNumeralTest {
+    
+    @ParameterizedTest
+    @CsvSource(value = {
+        "I:1",
+        "II:2"
+    })
+    void test(String romanNumeralStr, int arabicNumber) {
+        RomanNumeral romanNumeral = new RomanNumeral(arabicNumber);
+        assertEquals(romanNumeralStr, romanNumeral.toString());
+    }
 
     @Test
     void should_return_I_when_value_is_1() {
@@ -31,4 +44,5 @@ public class RomanNumeralTest {
         RomanNumeral romanNumeral = new RomanNumeral(5);
         assertEquals("V", romanNumeral.toString());
     }
+
 }
