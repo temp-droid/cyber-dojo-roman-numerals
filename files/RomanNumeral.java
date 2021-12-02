d4bfae07f277db8b0ce6f03f1c446baf992739a0
yVyqq9 <yVyqq9@cyber-dojo.org>
2021-12-02 19:43:18 +0000

    3 ran tests, predicted red, got amber

---
+++ files/RomanNumeral.java
@@ -0,0 +1,11 @@
+
+public class RomanNumeral {
+
+    public RomanNumeral(String arabicNumeral) {
+        this.arabicNumeral = arabicNumeral;
+    }
+    
+    public String toString() {
+        return null;   
+    }
+}
--- files/RomanNumeralTest.java
+++ files/RomanNumeralTest.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class HikerTest {
+public class RomanNumeralTest {
 
     @Test
-    void life_the_universe_and_everything() {
-        int expected = 42;
-        int actual = Hiker.answer();
-        assertEquals(expected, actual);
+    void test() {
+        RomanNumeral romanNumeral = new RomanNumeral(1);
+        assertEquals("I", romanNumeral.toString());
     }
 }
