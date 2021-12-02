7904e2d3f83553d980d3ab950ec8d8dd2c5506c4
yVyqq9 <yVyqq9@cyber-dojo.org>
2021-12-02 19:45:23 +0000

    7 ran tests, predicted red, got red

--- files/RomanNumeralTest.java
+++ files/RomanNumeralTest.java
@@ -9,8 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RomanNumeralTest {
 
     @Test
-    void test() {
+    void should_return_I_when_value_is_1() {
         RomanNumeral romanNumeral = new RomanNumeral(1);
         assertEquals("I", romanNumeral.toString());
+    }
+    
+    @Test
+    void should_return_II_when_value_is_2() {
+        RomanNumeral romanNumeral = new RomanNumeral(2);
+        assertEquals("II", romanNumeral.toString());
     }
 }
